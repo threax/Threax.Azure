dddbe27a4e3ed934389401182e9e95846ad2d2ee
Andrew Piper <andrew@threax.com>
2020-07-09 12:10:53 -0400

    Add project files.

---
+++ Threax.Configuration.AzureKeyVault/IConfigurationBuilderExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System;
+using Threax.Configuration.AzureKeyVault;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class IConfigurationBuilderExtensions
+    {
+        public static void AddThreaxKeyVaultConfig(this IConfigurationBuilder config, String keyVaultSection = "KeyVault")
+        {
+            var builtConfig = config.Build();
+            var keyVaultConfig = new ThreaxAzureKeyVaultConfig();
+            builtConfig.GetSection(keyVaultSection).Bind(keyVaultConfig);
+
+            if (keyVaultConfig.Enabled)
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                //This is disposable, but if you dispose it the config won't work. The MS docs show it this way so...
+                var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                IKeyVaultSecretManager manager;
+                if (string.IsNullOrEmpty(keyVaultConfig.Prefix))
+                {
+                    manager = new DefaultKeyVaultSecretManager();
+                }
+                else
+                {
+                    manager = new PrefixKeyVaultSecretManager(keyVaultConfig.Prefix);
+                }
+
+                config.AddAzureKeyVault($"https://{keyVaultConfig.VaultName}.vault.azure.net/", keyVaultClient, manager);
+            }
+        }
+    }
+}
---
+++ Threax.Configuration.AzureKeyVault/SchemaBinderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System;
+using Threax.Configuration.AzureKeyVault;
+using Threax.Extensions.Configuration.SchemaBinder;
+
+namespace Threax.Extensions.Configuration.SchemaBinder
+{
+    public static class SchemaBinderExtensions
+    {
+        public static void AddThreaxKeyVaultConfigDefinition(this SchemaConfigurationBinder config, String keyVaultSection = "KeyVault")
+        {
+            config.Define(keyVaultSection, typeof(ThreaxAzureKeyVaultConfig));
+        }
+    }
+}
