47b7dea1efffea1a8a84affb3f17e5d1d952f8ea
Andrew Piper <andrew@threax.com>
2020-07-27 14:16:56 -0400

    Finished adding app insights and removed comment warnings.

--- Threax.Azure.Abstractions/AzureAppInsightsConfig.cs
+++ Threax.Azure.Abstractions/AzureAppInsightsConfig.cs
@@ -7,7 +7,7 @@ namespace Threax.Azure.Abstractions
     public class AzureAppInsightsConfig
     {
         /// <summary>
-        /// Set this to true to enable app insights.
+        /// Set this to true to enable app insights. This is processed by the dependency injection extension method, so it can always be called.
         /// </summary>
         public bool Enabled { get; set; }
 
@@ -15,5 +15,31 @@ namespace Threax.Azure.Abstractions
         /// The instrumentation key to use for connecting to app insights. Default null.
         /// </summary>
         public String InstrumentationKey { get; set; }
+
+        /// <summary>
+        /// The Cloud Role Name for this app instance. Default null
+        /// </summary>
+        /// <remarks>
+        /// See https://docs.microsoft.com/en-us/azure/azure-monitor/app/app-map?tabs=net#set-cloud-role-name
+        /// </remarks>
+        public string RoleName { get; set; }
+
+        /// <summary>
+        /// The Cloud Role Instance Name for this app instance. Default: null
+        /// </summary>
+        /// <remarks>
+        /// See https://docs.microsoft.com/en-us/azure/azure-monitor/app/app-map?tabs=net#set-cloud-role-name
+        /// </remarks>
+        public string RoleInstance { get; set; }
+  
+        /// <summary>
+        /// Gets or sets a value indicating whether telemetry channel should be set to developer mode. Default: null
+        /// </summary>
+        public bool? DeveloperMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the application version reported with telemetries. Default: null
+        /// </summary>
+        public string ApplicationVersion { get; set; }
     }
 }
--- Threax.Azure.ApplicationInsights/DiExtensions.cs
+++ Threax.Azure.ApplicationInsights/DiExtensions.cs
@@ -1,14 +1,38 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using Threax.Azure.Abstractions;
+using Threax.Azure.ApplicationInsights;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class DiExtensions
     {
-        public static IServiceCollection AddThreaxAzureStorageDataProtection(this IServiceCollection services, Action<AzureAppInsightsConfig> configure)
+        /// <summary>
+        /// Add application insights using the config from the Threax.Azure.Abstractions library.
+        /// Optionally you can also include a callback that is called when app insights is configured, which exposes more options.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configure"></param>
+        /// <param name="configureAppInsights"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddThreaxAppInsights(this IServiceCollection services, Action<AzureAppInsightsConfig> configure, Action<ApplicationInsightsServiceOptions> configureAppInsights = null)
         {
-            
+            var options = new AzureAppInsightsConfig();
+            configure.Invoke(options);
+
+            if (options.Enabled)
+            {
+                services.AddSingleton<ITelemetryInitializer>(s => new TelemetryInitializer(options.RoleName, options.RoleInstance));
+                services.AddApplicationInsightsTelemetry(o =>
+                {
+                    o.InstrumentationKey = options.InstrumentationKey;
+                    o.DeveloperMode = options.DeveloperMode;
+                    o.ApplicationVersion = options.ApplicationVersion;
+                    configureAppInsights?.Invoke(o);
+                });
+            }
 
             return services;
         }
